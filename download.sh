a3e2819978539ec100dc153d142133f7947bab0e
anwen zhao <133937705+AnwZhao@users.noreply.github.com>
2025-07-01 02:00:59 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,11 @@
 git clone https://github.com/biomed-AI/GPSite
-mv GPSite/model/* ./surf2spot/data/model_gpsite
+mv GPSite/model/* ./Surf2Spot/data/model_gpsite
 git clone https://github.com/JudeWells/Chainsaw
-mv Chainsaw/saved_models/* ./surf2spot/data/chainsaw/saved_models
-cd ./surf2spot/data/model_emb
+mv Chainsaw/saved_models/* ./Surf2Spot/data/chainsaw/saved_models
+cd ./Surf2Spot/data/model_emb
 git clone https://huggingface.co/Rostlab/prot_t5_xl_half_uniref50-enc
 rm -rf GPSite
 rm -rf Chainsaw
-export PATH=$PATH:$(pwd)/surf2spot/data/feature_extraction/
+export PATH=$PATH:$(pwd)/Surf2Spot/data/feature_extraction/
+chmod +x -R $(pwd)/Surf2Spot
+
